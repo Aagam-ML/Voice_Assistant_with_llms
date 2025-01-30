992a3ac6c4695e86dd432b733f5c9a4faadd3636
Aagam Shah <151572822+Aagam-ML@users.noreply.github.com>
2025-01-30 22:43:55 +0100

    This Update will focus on changing the reminder as per the user needs

--- Automation/Activation.py
+++ Automation/Activation.py
@@ -53,9 +53,8 @@ class Voice_Assistant():
             with sr.Microphone() as source:
                 text = ""
                 print("Microphone is active. Speak now!")
-                while True:  # Infinite loop to keep the microphone on
+                while Tr‚ue:  # Infinite loop to keep the microphone on
                     try:
-
                         print("Listening...")
 
                         audio = recognizer.listen(source)  # Continuously listen
